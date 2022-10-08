d6251f3e7ce0cb2d897a49618c6044006ef9db9c
carag37 <carag37@gmail.com>
2022-10-08 18:31:34 -0500

    Avance Front Reto 3

--- R3/R3_G42_CR/src/main/java/com/example/demo/Interface/CategoryInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Interface;
-
-import com.example.demo.Modelo.Category;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface CategoryInterface extends CrudRepository<Category, Integer>{
-    
-}
